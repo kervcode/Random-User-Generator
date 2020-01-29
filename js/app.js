47e35add5414f7cbab4abc5f5ba35c1f4dd237fd
Kervcodes-desktop <Kervintz84@hotmail.com>
2020-01-28 23:19:40 -0500

    starting to manipulate data

--- js/app.js
+++ js/app.js
@@ -1,22 +1,20 @@
-(function getUsers(){
-    const URL = 'https://randomuser.me/api/?results=3';
+const URL = 'https://randomuser.me/api/?results=13';
 
-        fetch(URL)
-            .then(res => res.json())
-            .then(data => console.log(data.results))
-            // .then((data => {
-            //     data.forEach((user) => {
-            //         console.log(user.gender)
-            //     })
-            // }));
-})();
+fetch(URL)
+  .then(res => res.json())
+  // .then(data => console.log(data))
+  .then(data => data.results.map(user => generateUserProfile(user)))
 
 
+function generateUserProfile(data) {
+  const image = data.picture.medium;
+  const fullName = `${data.name.first} ${data.name.last}`;
+  const email = data.email;
+  const city = data.location.city;
 
+  console.log(image)
+  console.log(fullName)
+  console.log(email)
+  console.log(city)
 
-
-
-//helper functions
-// const results = (data) = {
-//     return 
-// }
+}
